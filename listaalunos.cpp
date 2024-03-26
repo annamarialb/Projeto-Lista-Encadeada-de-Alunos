a81a5e8e9db3cadf2251547b50141ff1085417d0
Maria Eduarda <142428634+iduarda@users.noreply.github.com>
2024-03-26 15:52:29 -0300

    cadastro aluno

--- listaalunos.cpp
+++ listaalunos.cpp
@@ -3,13 +3,13 @@
 #include <locale.h>
 #include <string.h>
 
-#define MAX 10
+#define MAX 60
 
 // Projeto Lista sequencial e encadeada: Lista Alunos
 
 typedef struct aluno{
 	char nome[50];
-	int rgm;
+	char rgm[20];
 }Aluno;
 
 typedef struct alunos{
@@ -21,7 +21,6 @@ typedef struct Disciplina {
     char nome[50];
     struct Disciplina *proxima;
 } Disciplina;
-
 
 Alunos criar() {
 	Alunos alunos;
@@ -43,6 +42,103 @@ int getTamanho(Alunos * alunos) {
 	return alunos->n + 1;
 }
 
+int deslocaDireita(Alunos * alunos, int pos) {
+    int i;
+
+    for (i=alunos->n + 1; i>pos; i--)
+        alunos->vetor[i] = alunos->vetor[i-1];
+
+    return 1;
+}
+
+int inserir (Alunos * alunos, int pos, Aluno dado) {
+    if ( isCheia(alunos) || (pos > alunos->n + 1) || (pos < 0) )
+        return 0;
+
+    deslocaDireita(alunos, pos);
+    alunos->vetor[pos] = dado;
+    (alunos->n)++;
+    return 1;
+}
+
+int inserirOrdenado(Alunos * alunos, Aluno dado) {
+	if(isCheia(alunos))
+		return 0;
+	
+	int pos = 0;
+		
+	while(pos <= alunos->n && compara(alunos->vetor[pos], dado) < 0)
+		pos++;
+		
+	if(pos <= alunos->n)
+		deslocaDireita(alunos, pos);
+		
+	alunos->vetor[pos] = dado;
+	(alunos->n)++;
+	
+	return 1;
+}
+
+int deslocaEsquerda(Alunos * alunos, int pos) {
+    int i;
+
+    for (i=pos; i<(alunos->n); i++)
+        alunos->vetor[i] = alunos->vetor[i+1];
+
+    return 1;
+}
+
+int remover (Alunos * alunos, int pos) {
+    if ( (pos > alunos->n) || (pos < 0) )
+        return 0;
+
+    deslocaEsquerda(alunos, pos);
+
+    (alunos->n)--;
+    return 1;
+}
+
+Aluno * getElemento(Alunos * alunos, int pos) {
+    if ( (pos > alunos->n) || (pos < 0) )
+        return 0;
+
+    return &(alunos->vetor[pos]);
+}
+
+int compara(Aluno dado1, Aluno dado2) {
+    return strcmp(dado1.rgm, dado2.rgm);
+}
+
+int getPosicao(Alunos * alunos, Aluno dado) {
+    int i;
+    
+    for (i=0; i<=alunos->n; i++)
+        if (compara(alunos->vetor[i], dado)==0)
+            return i;
+
+    return -1;
+}
+
+void mostrar(Alunos * alunos) {
+	int	i;
+	
+	for (i = 0; i <= alunos->n; i++) {
+		printf("\nAluno [%d] com: RGM: [%s] | Nome: [%s]\n", i + 1, alunos->vetor[i].rgm, alunos->vetor[i].nome);
+	}
+}
+
+int buscaSequencial(Alunos * alunos, char rgm[]) {
+    int i;
+    for (i = 0; i <= alunos->n; i++) {
+        if (strcmp(alunos->vetor[i].rgm, rgm) == 0) {
+            printf("\n---RGM encontrado na posição %d.---\n", i + 1);
+            return i;
+        }
+    }
+    printf("RGM do aluno não existe.\n");
+    return -1;
+}
+
 int lerNumero() { //função para ler a entrada do usuário
     
     int numero;
@@ -56,49 +152,73 @@ int main (int argc, char *argv[]) {
 	setlocale (LC_ALL, "portuguese");
 	
 	Alunos meusalunos;
+	Aluno aluno;
 	
 	meusalunos = criar();
 	
+	int pos;
 	int loop = 1;
 	int numero = 0;
-	
+	char nomeBusca[20];
 	while(loop == 1){
-    	system("cls");
+    system("cls");
 	printf("\n\t\n\n");
 	printf("1. Cadastrar alunos\n");
 	printf("2. Listar alunos\n");
-	printf("3. Buscar alunos\n");
-	printf("4. Sair\n\n");
+	printf("3. Buscar aluno\n");
+	printf("4. Remover aluno\n");
+	printf("5. Sair\n\n");
 	printf("Digite o número da opção que deseja: ");
 	numero = lerNumero();
     	
 	switch (numero) {
             case 1:{
-                printf("Cadastrar aluno: ");
-		break;
-		}
+                printf("\n-----Cadastrando aluno-----\n");
+                printf("\nInforme o nome: ");
+                scanf("%s", aluno.nome);
+                printf("Informe o RGM: ");
+                scanf("%s", aluno.rgm);
+                inserirOrdenado(&meusalunos, aluno);
+				break;
+			}
             case 2:{
-               	printf("Listar alunos: ");
-		break;
-		}
+               	printf("\n-----Listando alunos-----\n");
+                mostrar(&meusalunos);
+				break;
+			}
             case 3:{
-                printf("Buscar alunos: ");
-		break;
-		}
-            case 4:{
+                printf("\n-----Buscando aluno-----\n");
+                printf("\nInforme o RGM que deseja buscar: ");
+                scanf("%s", nomeBusca);
+                buscaSequencial(&meusalunos, nomeBusca);
+				break;
+			}
+			case 4:{
+				printf("\n-----Removendo aluno-----\n");
+				
+				mostrar(&meusalunos);
+				
+				printf("\nInforme a posicao do nome que deseja apagar: ");
+                scanf("%d", &pos);
+                remover(&meusalunos, pos - 1);
+                
+                mostrar(&meusalunos);
+				break;
+			}
+            case 5:{
                 printf("\nSaindo...");
                 loop = 0;
-		break;
-		}
-	    default:{
-		printf("\nOpção Inválida!\n");
-		break;
-		}
+				break;
+			}
+	    	default:{
+				printf("\nOpção Inválida!\n");
+				break;
+			}
         }
         if (loop != 0) {
 		printf("\nDigite '1' para voltar ao menu, ou '0' para encerrar o programa.\n");
 		loop = lerNumero();
-	}
+		}
 	}
 	return 0;
 }
